aff9757b12197215238d6aa3badfbc9fe909f9d2
Oran <baroran@hotmail.com>
2019-11-29 18:40:01 +0100

    ?

---
+++ app/src/main/java/com/example/testinggrounds/PlayPauseWPShuffle.java
@@ -0,0 +1,14 @@
+package com.example.testinggrounds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayPauseWPShuffle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_pause_wpshuffle);
+    }
+}
